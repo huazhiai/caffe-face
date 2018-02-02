7b7331646d799bec2088b564cc6a28ebaae10ba5
chaohua zhu <34745140+chaohuazhu@users.noreply.github.com>
2018-02-02 18:47:02 +0800

    Update extractDeepFeature.m

--- face_example/extractDeepFeature.m
+++ face_example/extractDeepFeature.m
@@ -1,27 +1,30 @@
 clear;clc;
-addpath('path_to_matCaffe/matlab');
+
+load('LFW_Feature.mat')
+
+addpath(genpath('d:/zhuch/caffe'));
 caffe.reset_all();
 
 % load face model and creat network
 caffe.set_device(0);
 caffe.set_mode_gpu();
-model = 'path_to_deploy/face_deploy.prototxt';
-weights = 'path_to_model/face_model.caffemodel';
+model = 'd:/zhuch/windows_centerloss_caffe/face_example/face_deploy.prototxt';
+weights = 'd:/zhuch/windows_centerloss_caffe/face_example/face_snapshot_iter_124000.caffemodel';
 net = caffe.Net(model, weights, 'test');
 
 % load face image, and align to 112 X 96
-imgSize = [112, 96];
-coord5points = [30.2946, 65.5318, 48.0252, 33.5493, 62.7299; ...
-                51.6963, 51.5014, 71.7366, 92.3655, 92.2041];
+%imgSize = [112, 96];
+%coord5points = [30.2946, 65.5318, 48.0252, 33.5493, 62.7299; ...
+%                51.6963, 51.5014, 71.7366, 92.3655, 92.2041];
 
-image = imread('path_to_image/Jennifer_Aniston_0016.jpg');
-facial5points = [105.8306, 147.9323, 121.3533, 106.1169, 144.3622; ...
-                 109.8005, 112.5533, 139.1172, 155.6359, 156.3451];
+%image = imread('d:/zhuch/windows_centerloss_caffe/face_example/Jennifer_Aniston_0016.jpg');
+%facial5points = [105.8306, 147.9323, 121.3533, 106.1169, 144.3622; ...
+%                 109.8005, 112.5533, 139.1172, 155.6359, 156.3451];
 
-Tfm =  cp2tform(facial5points', coord5points', 'similarity');
-cropImg = imtransform(image, Tfm, 'XData', [1 imgSize(2)],...
-                                  'YData', [1 imgSize(1)], 'Size', imgSize);
-
+%Tfm =  cp2tform(facial5points', coord5points', 'similarity');
+%cropImg = imtransform(image, Tfm, 'XData', [1 imgSize(2)],...
+%                                  'YData', [1 imgSize(1)], 'Size', imgSize);
+cropImg = imread('d:/zhuch/windows_centerloss_caffe/face_example/jen.jpg');
 % transform image, obtaining the original face and the horizontally flipped one
 if size(cropImg, 3) < 3
    cropImg(:,:,2) = cropImg(:,:,1);
@@ -42,3 +45,6 @@ res_ = net.forward({cropImg_});
 deepfeature = [res{1}; res_{1}];
 
 caffe.reset_all();
+all = deepfeature' * feature
+[value,pos]=max(all)
+name = list(pos)
